b295b0bf8e0a36bc9f4ecb2e8aea9bb9f730e870
Boris Rybalkin <ribalkin@gmail.com>
2024-01-08 16:19:33 +0000

    fix config

--- bin/service.frigate.sh
+++ bin/service.frigate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 cd ${DIR}/frigate/opt/frigate
+export CONFIG_FILE=/var/snap/frigate/current/config/frigate/config.yaml
 exec ${DIR}/frigate/bin/python.sh -u -m frigate
