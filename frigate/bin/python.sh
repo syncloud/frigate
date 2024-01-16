f7bb5369e54d1739812024c85a16a9e1410ebe8b
Boris Rybalkin <ribalkin@gmail.com>
2024-01-16 16:12:16 +0000

    fix config

--- frigate/bin/python.sh
+++ frigate/bin/python.sh
@@ -4,4 +4,5 @@ LIBS=$(echo ${DIR}/lib/*-linux-gnu*)
 LIBS=$LIBS:$(echo ${DIR}/usr/lib/*-linux-gnu*)
 LIBS=$LIBS:$(echo ${DIR}/usr/lib)
 LIBS=$LIBS:$(echo ${DIR}/usr/local/lib)
+export CONFIG_FILE=/var/snap/frigate/current/config/frigate/config.yaml
 ${DIR}/lib/*-linux*/ld-*.so.* --library-path $LIBS ${DIR}/usr/bin/python3.* "$@"
