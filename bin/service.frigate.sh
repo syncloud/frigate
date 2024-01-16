defa7599f2650fdd95845b927a8223ecb59c24e7
Boris Rybalkin <ribalkin@gmail.com>
2024-01-16 16:20:56 +0000

    disable test for now

--- bin/service.frigate.sh
+++ bin/service.frigate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 cd ${DIR}/frigate/opt/frigate
+export CONFIG_FILE=/var/snap/frigate/current/config/frigate/config.yaml
 export PATH=$PATH:${DIR}/frigate/bin
 exec ${DIR}/frigate/bin/python.sh -u -m frigate
--- frigate/bin/python.sh
+++ frigate/bin/python.sh
@@ -4,5 +4,4 @@ LIBS=$(echo ${DIR}/lib/*-linux-gnu*)
 LIBS=$LIBS:$(echo ${DIR}/usr/lib/*-linux-gnu*)
 LIBS=$LIBS:$(echo ${DIR}/usr/lib)
 LIBS=$LIBS:$(echo ${DIR}/usr/local/lib)
-export CONFIG_FILE=/var/snap/frigate/current/config/frigate/config.yaml
 ${DIR}/lib/*-linux*/ld-*.so.* --library-path $LIBS ${DIR}/usr/bin/python3.* "$@"
