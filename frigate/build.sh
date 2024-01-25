7e86f0931c8dcfb3949cfbdda8f2b8f957a50fba
Boris Rybalkin <ribalkin@gmail.com>
2024-01-25 15:20:18 +0000

    fix ffmpeg

--- frigate/build.sh
+++ frigate/build.sh
@@ -32,6 +32,6 @@ cp -r /lib ${BUILD_DIR}
 cp -r /*.txt ${BUILD_DIR}
 cp -r /*.tflite ${BUILD_DIR}
 cp -r /openvino-model ${BUILD_DIR}
-cp -r /usr/lib/btbn-ffmpeg/bin/* ${BUILD_DIR}/usr/bin
+cp -r /usr/lib/btbn-ffmpeg/bin/* ${BUILD_DIR}/usr/bin || true
 curl -o ${BUILD_DIR}/black.mp4 https://user-images.githubusercontent.com/362089/178692766-3a394ad7-e027-48cb-b2c3-1c6d7be2370e.mp4
 cp -r ${DIR}/bin/* ${BUILD_DIR}/bin
