0f3963a2873b763a1b969bdb3ddc745bafa55969
Boris Rybalkin <ribalkin@gmail.com>
2024-02-03 09:35:30 +0000

    test

--- frigate/test.sh
+++ frigate/test.sh
@@ -6,6 +6,6 @@ VERSION=$1
 BUILD_DIR=${DIR}/../build/snap/frigate
 cd $BUILD_DIR/opt/frigate
 #${BUILD_DIR}/bin/python.sh -u -m frigate --help
-
+mkdir -p /var/snap/frigate/current
 $BUILD_DIR/bin/ffmpeg --help
 $BUILD_DIR/bin/ffprobe --help
