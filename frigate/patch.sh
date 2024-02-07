03dd3087e9fd0ab270102e180d75bd02c58b4bf0
Boris Rybalkin <ribalkin@gmail.com>
2024-02-07 21:45:11 +0000

    fix patch again

--- frigate/patch.sh
+++ frigate/patch.sh
@@ -7,6 +7,5 @@ BUILD_DIR=${DIR}/../build/snap/frigate
 apk add patch
 
 wget https://github.com/blakeblackshear/frigate/compare/v$VERSION...cyberb:frigate:v$VERSION-fix.patch -O $DIR/fix.patch
-tar xf v$VERSION-patch.tar.gz
 cd $BUILD_DIR/opt/frigate/frigate
 patch -p0 < $DIR/fix.patch
